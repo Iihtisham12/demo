8dbc9697847d0adaee8b8dafe48be239ae934e28
Ihtishamul Haq <rajsultaan42@gmail.com>
2024-05-04 11:30:41 +0500

    I Add Simple Calculate

--- demo1.py
+++ demo1.py
@@ -1,1 +1,9 @@
 print("This is My Demo One Class")
+
+def simple_calculate_sum(sum):
+    if (1+6) == (sum):
+        print("The Sum Result is",sum)
+    else:
+        print("Sorry I Can't Recongnaize")
+
+simple_calculate_sum(7)
