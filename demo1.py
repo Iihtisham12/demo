56b6de79e1e8d1a17b493e5acfdb1f1025c28e5b
Ihtishamul Haq <rajsultaan42@gmail.com>
2024-05-04 11:44:05 +0500

    I add One Branch

--- demo1.py
+++ demo1.py
@@ -5,5 +5,11 @@ def simple_calculate_sum(sum):
         print("The Sum Result is",sum)
     else:
         print("Sorry I Can't Recongnaize")
-
 simple_calculate_sum(7)
+
+name = input('Enter Ur Name')
+try:
+    if int(name) // int(name) == 1:
+        print("Sorry Number is Not Allowd!")
+except:
+    print(f"Hillo {name.capitalize()}")
